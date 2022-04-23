9bbc10e00923fff48d721e741eb880cb53be97b8
Brooke Felsheim <brookefelsheim@gmail.com>
2022-04-23 14:24:21 -0400

    Helper function to check correct bike sharing data input

---
+++ package/bikeSharing/R/checkBikeData.R
@@ -0,0 +1,41 @@
+#' Check correct format of bike sharing data
+#'
+#' This is a helper function to ensure that the bike
+#' sharing dataset used as input to a function is
+#' properly formatted. Throws warnings if formatting
+#' is incorrect.
+#'
+#' @param data pre-processed bike sharing data frame
+#'             with columns Bike_count, Hour_chunks,
+#'             Max_temp, and Rain_or_snow
+#'
+checkBikeData <- function(data) {
+
+  if (!is.data.frame(data)) {
+    stop("Input data is not a data frame")
+  }
+  if (!all(c("Bike_count", "Hour_chunks", "Max_temp", "Rain_or_snow") %in%
+           colnames(seoul))) {
+    stop("Input data does not contain all necessary columns:
+         Bike_count, Hour_chunks, Max_temp, and Rain_or_snow")
+  }
+  if (!is.numeric(data$Bike_count)) {
+    stop("Bike_count column must be numeric")
+  }
+  if(length(levels(seoul$Hour_chunks)) != 3) {
+    stop("Hour_chunks column must contain exactly 3 levels")
+  }
+  if(!all(levels(seoul$Hour_chunks) == c("[0,8)", "[8,16)", "[16,24)"))) {
+    stop('Hour_chunks levels must be "[0,8)", "[8,16)", "[16,24)"')
+  }
+  if (!is.numeric(data$Max_temp)) {
+    stop("Max_temp column must be numeric")
+  }
+  if (!is.numeric(data$Rain_or_snow)) {
+    stop("Rain_or_snow column must be numeric")
+  }
+  if(!all(unique(data$Rain_or_snow) %in% c(0, 1))) {
+    stop("Rain_or_snow column must contain only values of 0 and 1")
+  }
+
+}
