3c85a568a0f1abb3d8a8fe459859ee4fa9cc0275
Brooke Felsheim <brookefelsheim@gmail.com>
2022-04-29 16:01:19 -0400

    Additional tests for random forest input

--- package/bikeSharing/tests/testthat/test-randomForest.R
+++ package/bikeSharing/tests/testthat/test-randomForest.R
@@ -1,4 +1,13 @@
-test_that("errors for bad input", {
-  expect_error(trainRandomForest(iris))
-  expect_error(trainRandomForest(matrix(1:16, nrow = 4)))
+test_that("errors for bad input to train_random_forest", {
+  expect_error(train_random_forest(iris))
+  expect_error(train_random_forest(matrix(1:16, nrow = 4)))
+  expect_error(train_random_forest("test"))
+  expect_error(train_random_forest())
+})
+
+test_that("errors for bad input to plot_rf_importance", {
+  expect_error(plot_rf_importance(iris))
+  expect_error(plot_rf_importance(matrix(1:16, nrow = 4)))
+  expect_error(plot_rf_importance("test"))
+  expect_error(plot_rf_importance())
 })
